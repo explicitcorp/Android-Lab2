c88ec0b6bb03d2e0e091952e2224057709174225
Adam O'Brien <obri0056@algonquinlive.com>
2020-09-29 15:14:10 -0400

    Changes

--- app/src/main/java/com/example/mylab2/MainActivity.java
+++ app/src/main/java/com/example/mylab2/MainActivity.java
@@ -2,15 +2,18 @@ package com.example.mylab2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.Toast;
 
+import com.google.android.material.snackbar.BaseTransientBottomBar;
 import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
@@ -30,15 +33,22 @@ public class MainActivity extends AppCompatActivity {
                 toast.show();
             }
             });
-        Snackbar onOff = Snackbar.make(findViewById(R.id.on_off_switch);
+       @SuppressLint("WrongViewCast")
+       Switch onOff = (Switch) findViewById(R.id.on_off_switch);
+        final Snackbar mySnackbar = Snackbar.make(findViewById(R.id.linLay),R.string.SnackMessageOn , BaseTransientBottomBar.LENGTH_INDEFINITE);
+        final Snackbar mySnackbar = Snackbar.make(findViewById(R.id.linLay),R.string.SnackMessageOff , BaseTransientBottomBar.LENGTH_INDEFINITE);
         onOff.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-
+            public void onCheckedChanged(CompoundButton cb, boolean b) {
+                mySnackbar.setAction( "Undo", click -> cb.setChecked(!b));
+              if(!b){
+                  mySnackbar.setText("off");
+                  mySnackbar.show();
+                  else{
+                      mySnackbar.setText("on");
+                      mySnackbar.show();
+                  }
             }
+        });
         }
-public void onCheckChanged(CompoundButton cb, boolean b){
-            mySnackbar.setAction(R.string.undo_string, new MyUndoListener());
-        mySnackbar.show();
     }
 }
