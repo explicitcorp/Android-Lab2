25abf955677971ab42fac444cc8cd41d8b0e1304
Adam O'Brien <obri0056@algonquinlive.com>
2020-09-29 22:02:14 -0400

    step3

--- app/src/main/java/com/example/mylab2/MainActivity.java
+++ app/src/main/java/com/example/mylab2/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.mylab2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
@@ -21,30 +20,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.relative_layout);
         final Button button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Context context = getApplicationContext();
-                CharSequence text = getResources().getString(R.string.Toast);
-                int duration = Toast.LENGTH_LONG;
+        button.setOnClickListener(v -> {
+            Context context = getApplicationContext();
+            CharSequence text = getResources().getString(R.string.Toast);
+            int duration = Toast.LENGTH_LONG;
 
-                Toast toast = Toast.makeText(context, text, duration);
-                toast.show();
-            }
-            });
-       @SuppressLint("WrongViewCast")
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        });
+
        Switch onOff = (Switch) findViewById(R.id.on_off_switch);
         final Snackbar snackOn = Snackbar.make(findViewById(R.id.linLay),R.string.SnackMessageOn , BaseTransientBottomBar.LENGTH_INDEFINITE);
         final Snackbar snackOff = Snackbar.make(findViewById(R.id.linLay),R.string.SnackMessageOff , BaseTransientBottomBar.LENGTH_INDEFINITE);
-        onOff.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            public void onCheckedChanged(CompoundButton cb, boolean b) {
-                snackOn.setAction( "Undo", click -> cb.setChecked(!b));
-                snackOff.setAction( "Undo", click -> cb.setChecked(!b));
-              if(b){
-                  snackOn.show();}
-                  else{
-                      snackOff.show();
-                  }
-            }
+        onOff.setOnCheckedChangeListener((cb, b) -> {
+            snackOn.setAction( "Undo", click -> cb.setChecked(!b));
+            snackOff.setAction( "Undo", click -> cb.setChecked(!b));
+          if(b){
+              snackOn.show();}
+              else{
+                  snackOff.show();
+              }
         });
         }
     }
